4e6b4b1388049fe95062042ba87e7b28e8970f37
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-08-15 01:09:00 +0300

    feat: add failed to admin migration file

--- database/migrations/2025_08_14_213415_create_admins_table.php
+++ database/migrations/2025_08_14_213415_create_admins_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone')->nullable();
+            $table->boolean('verified')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
