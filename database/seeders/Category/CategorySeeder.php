6c582990a0e0c48d692a3efa569b67ce38c42d8f
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-08-15 18:13:07 +0300

    feat: add category migration, model, and seeder

---
+++ database/seeders/Category/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders\Category;
+
+use App\Models\Category\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Electronics',
+            'slug' => 'electronics',
+            'image' => 'image.jpg',
+            'description' => 'Electronics description',
+        ]);
+    }
+}
