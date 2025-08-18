e69af016e27f0b16060ec5a063629f7a905fb543
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-08-18 19:22:01 +0300

    fix: add unique constraint in category translation

--- database/migrations/2025_08_18_151146_create_category_translations_table.php
+++ database/migrations/2025_08_18_151146_create_category_translations_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->enum('local', ['en', 'ar']);
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
+            $table->unique(['category_id', 'local']);
             $table->timestamps();
         });
     }
