d56b64148a5e87d7d5cbfbb6b62d4433e9cacb58
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-09-11 23:54:19 +0300

    fix: regenerate isDeleivred method because mistake removed

--- app/Repositories/interface/Order/OrderRepositoryInterface.php
+++ app/Repositories/interface/Order/OrderRepositoryInterface.php
@@ -8,6 +8,8 @@ interface OrderRepositoryInterface
 {
   public function store(array $data): Order;
 
+  public function isDelivered(int $orderId): bool;
+
   public function countCouponUsage(int $couponId): int;
 
   public function countUserCouponUsage(int $couponId, int $userId): int;
