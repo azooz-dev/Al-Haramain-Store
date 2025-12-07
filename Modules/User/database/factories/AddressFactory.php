891fb34ab412eb41e4a592487d58900ddd5f8d30
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-07 14:58:46 +0300

    fix(user): explicitly set model property in AddressFactory
    - Add protected \ property to AddressFactory
    - Ensures factory uses Modules\User\Entities\Address

--- Modules/User/database/factories/AddressFactory.php
+++ Modules/User/database/factories/AddressFactory.php
@@ -12,6 +12,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class AddressFactory extends Factory
 {
   /**
+   * The name of the factory's corresponding model.
+   *
+   * @var class-string<\Illuminate\Database\Eloquent\Model>
+   */
+  protected $model = Address::class;
+
+  /**
    * Define the model's default state.
    *
    * @return array<string, mixed>
