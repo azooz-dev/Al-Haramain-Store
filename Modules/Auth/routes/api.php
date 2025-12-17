cfb9f3664cf81bae53124824e7312935ce62b71c
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-17 18:44:46 +0300

    feat(auth): create Auth service providers
    - Create AuthServiceProvider with repository bindings
    - Register event listeners in EventServiceProvider
    - Create RouteServiceProvider for API routes
    - Move auth routes to Modules/Auth/routes/api.php

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Auth\Http\Controllers\AuthController;
+use Illuminate\Session\Middleware\StartSession;
+use Modules\Auth\Http\Controllers\ResetPasswordController;
+use Modules\Auth\Http\Controllers\ForgetPasswordController;
+use Modules\Auth\Http\Controllers\EmailVerificationController;
+use Modules\Auth\Http\Controllers\ResendEmailVerificationController;
 
-// Add your API routes here
-// Example:
-// Route::apiResource('auths', AuthController::class)->only(['index', 'show']);
+Route::middleware([StartSession::class, 'set.locale'])->group(function () {
+  Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::get('user', [AuthController::class, 'user']);
+  });
+
+  // Auth Routes
+  Route::post('login', [AuthController::class, 'login']);
+  Route::post('register', [AuthController::class, 'register']);
+  Route::post('users/email/verify-code', [EmailVerificationController::class, 'verify']);
+  Route::post('users/email/resend-code', [ResendEmailVerificationController::class, 'resend'])->middleware('throttle:3,1');
+  Route::post("/forget-password", [ForgetPasswordController::class, 'forget']);
+  Route::post("/reset-password", [ResetPasswordController::class, 'reset']);
+});
