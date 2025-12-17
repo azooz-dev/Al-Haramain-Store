a65c8607c84e3c9c7370f158f35f131ba0619f19
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-18 02:42:51 +0300

    refactor(analytics): remove Analytics bindings from AppServiceProvider
    - Remove Analytics repository bindings (moved to AnalyticsServiceProvider)
    - Remove unused Analytics-related imports
    - Analytics bindings now handled by AnalyticsServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,14 +14,6 @@ use Illuminate\Support\Facades\Event;
 
 use Illuminate\Support\ServiceProvider;
 use Modules\Catalog\Entities\Product\ProductColorImage;
-use App\Repositories\Eloquent\Analytics\UserAnalyticsRepository;
-use App\Repositories\Eloquent\Analytics\OrderAnalyticsRepository;
-use App\Repositories\Eloquent\Analytics\ProductAnalyticsRepository;
-use App\Repositories\Eloquent\Analytics\CategoryAnalyticsRepository;
-use App\Repositories\Interface\Analytics\UserAnalyticsRepositoryInterface;
-use App\Repositories\Interface\Analytics\OrderAnalyticsRepositoryInterface;
-use App\Repositories\Interface\Analytics\ProductAnalyticsRepositoryInterface;
-use App\Repositories\Interface\Analytics\CategoryAnalyticsRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,23 +24,6 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register Repository Bindings
         $this->app->bind(AdminRepositoryInterface::class, AdminRepository::class);
-
-        $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
-
-        $this->app->bind(EmailVerificationRepositoryInterface::class, EmailVerificationRepository::class);
-        $this->app->bind(ResendEmailVerificationRepositoryInterface::class, ResendEmailVerificationRepository::class);
-        $this->app->bind(ForgetPasswordRepositoryInterface::class, ForgetPasswordRepository::class);
-        $this->app->bind(ResetPasswordRepositoryInterface::class, ResetPasswordRepository::class);
-
-
-
-
-        // Register Analytics Repository Bindings
-        $this->app->bind(UserAnalyticsRepositoryInterface::class, UserAnalyticsRepository::class);
-        $this->app->bind(OrderAnalyticsRepositoryInterface::class, OrderAnalyticsRepository::class);
-        $this->app->bind(ProductAnalyticsRepositoryInterface::class, ProductAnalyticsRepository::class);
-
-        $this->app->bind(CategoryAnalyticsRepositoryInterface::class, CategoryAnalyticsRepository::class);
 
     }
 
