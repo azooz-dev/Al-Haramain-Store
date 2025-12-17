7a00e034d836680bad24e8acf347254f8803ff6e
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-17 15:58:42 +0300

    refactor(favorite): move FavoriteApiResource to Favorite module
    - Move FavoriteApiResource to Modules\Favorite\Http\Resources\Favorite
    - Update namespace
    - Preserve git history using git mv

--- Modules/Favorite/app/Http/Resources/Favorite/FavoriteApiResource.php
+++ Modules/Favorite/app/Http/Resources/Favorite/FavoriteApiResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Resources\Favorite;
+namespace Modules\Favorite\Http\Resources\Favorite;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
