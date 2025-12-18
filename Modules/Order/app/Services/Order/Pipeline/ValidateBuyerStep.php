eee5be2ed27cb006c590386cc4ffa801ae024e0d
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-19 02:06:06 +0300

    refactor(order): use UserServiceInterface in ValidateBuyerStep

--- Modules/Order/app/Services/Order/Pipeline/ValidateBuyerStep.php
+++ Modules/Order/app/Services/Order/Pipeline/ValidateBuyerStep.php
@@ -2,14 +2,16 @@
 
 namespace Modules\Order\Services\Order\Pipeline;
 
-use Modules\User\Entities\User;
+use Modules\User\Contracts\UserServiceInterface;
 use Modules\Order\Exceptions\Order\OrderException;
 
 class ValidateBuyerStep implements OrderProcessingStep
 {
+    public function __construct(private UserServiceInterface $userService) {}
+
     public function handle(array $data, \Closure $next)
     {
-        $buyer = User::find($data['user_id']);
+        $buyer = $this->userService->findUserById($data['user_id']);
         
         if (!$buyer) {
             throw new OrderException(__('app.messages.order.buyer_not_found'), 404);
