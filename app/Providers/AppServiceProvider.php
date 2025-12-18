24cb50830e78f1bb90b72372d05aafd8806531bc
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-18 03:37:02 +0300

    fix: add missing imports to AppServiceProvider
    - Add AdminRepositoryInterface and AdminRepository imports
    - Add Favorite and FavoritePolicy imports
    - Fix binding and policy registration references

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,10 @@ namespace App\Providers;
 use Modules\Catalog\Entities\Category\Category;
 use Modules\Catalog\Entities\Product\Product;
 use Illuminate\Support\Facades\Gate;
+use Modules\Admin\Repositories\Interface\AdminRepositoryInterface;
+use Modules\Admin\Repositories\Eloquent\AdminRepository;
+use Modules\Favorite\Entities\Favorite\Favorite;
+use Modules\Favorite\Policies\Favorite\FavoritePolicy;
 
 
 
@@ -24,7 +28,6 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register Repository Bindings
         $this->app->bind(AdminRepositoryInterface::class, AdminRepository::class);
-
     }
 
     /**
