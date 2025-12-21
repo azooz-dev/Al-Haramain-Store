496be87e50b5271cb09ca09bb9cb0ce26973f091
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-21 19:23:45 +0300

    fix(order): use ProductVariantServiceInterface in OrderRequest
    - Replace concrete ProductVariantService with ProductVariantServiceInterface
    - Fixes HMVC violation by using interface instead of concrete class

--- Modules/Order/app/Http/Requests/Order/OrderRequest.php
+++ Modules/Order/app/Http/Requests/Order/OrderRequest.php
@@ -4,11 +4,11 @@ namespace Modules\Order\Http\Requests\Order;
 
 use Modules\Payment\Enums\PaymentMethod;
 use Modules\Order\Http\Requests\Order\BaseOrderRequest;
-use Modules\Catalog\Services\Product\Variant\ProductVariantService;
+use Modules\Catalog\Contracts\ProductVariantServiceInterface;
 
 class OrderRequest extends BaseOrderRequest
 {
-    public function __construct(private ProductVariantService $variantService) {}
+    public function __construct(private ProductVariantServiceInterface $variantService) {}
 
     /**
      * Get the validation rules that apply to the request.
