a185e9aab409065580e6e2a7580d73b0e44c796d
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-24 23:13:12 +0300

    fix: correct test assertions and factory model property

--- Modules/Order/database/factories/Order/OrderFactory.php
+++ Modules/Order/database/factories/Order/OrderFactory.php
@@ -16,6 +16,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class OrderFactory extends Factory
 {
   /**
+   * The name of the factory's corresponding model.
+   *
+   * @var string
+   */
+  protected $model = Order::class;
+
+  /**
    * Define the model's default state.
    *
    * @return array<string, mixed>
