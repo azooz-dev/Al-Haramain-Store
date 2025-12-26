e630896a2e0af2e24c0636d7d93ed7473e82a7bd
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-12-26 04:36:28 +0300

    feat: Added redis server to optimize the queue and cache process

--- config/database.php
+++ config/database.php
@@ -147,7 +147,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'Al-Haramain-store')).'-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
@@ -167,6 +167,15 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+        'queue' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_QUEUE_DB', '2'),
         ],
 
     ],
