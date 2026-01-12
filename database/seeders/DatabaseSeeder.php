9450c022d7ac66ec307403d7d356ea4bf32820a0
root <root@Azooz.localdomain>
2026-01-12 10:19:45 +0300

    return nacessery seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
 
             // Module seeders - Order matters for foreign key constraints
+            UserDatabaseSeeder::class,
             AdminDatabaseSeeder::class,
             CatalogDatabaseSeeder::class, // Categories, Products, Variants, Colors, Images, Translations
             OfferDatabaseSeeder::class,   // Offers, Offer Translations, Offer Products
